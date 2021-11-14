b6703e108fdbad7eedff4020c33d2877334c44cf
Ahmad Taheri <92728743+AhmadTaheri2021@users.noreply.github.com>
2021-11-15 02:59:18 +0330

    Update Server_side.py

--- Server_side.py
+++ Server_side.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 
 '''
-The following code is an implementation of the Federated Deep Learning. 
+The following code is the server side implementation and consists of several functions such as
+config_(), create_model(), Aggregation() and evaluation(). 
 
-Copyright 2021 Ahmad Taheri. All Rights Reserved.
+      Copyright 2021 Ahmad Taheri. All Rights Reserved.
+      
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
