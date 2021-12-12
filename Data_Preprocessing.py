e4e57a81702fe9b3b8c6b149cc92424084cb56d9
Ahmad Taheri <92728743+AhmadTaheri2021@users.noreply.github.com>
2021-12-12 14:38:22 +0330

    Update Data_Preprocessing.py

--- Data_Preprocessing.py
+++ Data_Preprocessing.py
@@ -20,19 +20,36 @@ limitations under the License.
 '''
 
 import numpy as np
-import random
+import pandas as pd
+import matplotlib.pyplot as plt
 import tensorflow as tf
 
 # #### import and prepare data ####
-def load_detaset():
+def load_detaset(global_config):
+    dataset_ = global_config['dataset_']
     num_of_classes = 10
     input_shape = (28, 28, 1)
-
+    print(' loading  {}  Dataset...'.format(dataset_))
     # laod dataset 
-   #(X_train, Y_train), (X_test, Y_test) = tf.keras.datasets.mnist.load_data()
-    (X_train, Y_train), (X_test, Y_test) = tf.keras.datasets.fashion_mnist.load_data()
+    if(dataset_ == 'MNIST'):
+       (X_train, Y_train), (X_test, Y_test) = tf.keras.datasets.mnist.load_data()
+       num_of_classes = 10
+       input_shape = (28, 28, 1)
     
-    return X_train, Y_train , X_test, Y_test
+    if(dataset_ == 'FMNIST'):
+       (X_train, Y_train), (X_test, Y_test) = tf.keras.datasets.fashion_mnist.load_data()   
+       num_of_classes = 10
+       input_shape = (28, 28, 1)
+
+    if(dataset_ == 'CFAR10'):
+       (X_train, Y_train), (X_test, Y_test) = tf.keras.datasets.cifar10.load_data()   
+       num_of_classes = 10
+       input_shape = (32, 32, 3)
+
+
+    global_config.update({'num_of_classes' : num_of_classes})
+    global_config.update({'input_shape' : input_shape})
+    return X_train, Y_train , X_test, Y_test, global_config
 
 # ###  Scaling data ###
 def Scaling_data(X_train, Y_train , X_test, Y_test):
@@ -51,41 +68,212 @@ def Scaling_data(X_train, Y_train , X_test, Y_test):
     return X_train, Y_train , X_test, Y_test
 
 # ###### preparing disributed data  ######
-def Data_partitioning(data, num_of_clients,client_names,max_sample_size):
- 
+def Data_partitioning(X_train,Y_train, global_config,show_dist=True):
+  
+    data = list(zip(X_train, Y_train))
+    num_of_classes = int(np.asscalar(np.max(Y_train))+1)
+    num_of_clients = global_config['num_of_clients']
+    client_names = global_config['client_names']
+    dist_type = global_config['dist_type']
+    alpha = global_config['dist_alpha']
+    
     # shuffling the data (randomization)  
-    random.shuffle(data)
-    
-    np.random.shuffle(client_names)
-
-    # the minimum sample size is 0.2% of total samples
-    min_sample_size = len(data)*2//1000 
-    Sizes = [min_sample_size for i in range(0,num_of_clients)] 
-
-    R_samps = len(data) - (min_sample_size * num_of_clients )
-    #z = np.random.randint(0,num_of_clients)
-    z = 0
-    while R_samps > (len(data)*0.001):
-       clientID = z # np.random.randint(z,num_of_clients)
-       samp_size = int(np.round( R_samps * max_sample_size * np.random.rand()))
-       Sizes[clientID] += samp_size     
-       R_samps = R_samps - samp_size
-       z += 1
-       if z == 100 :
-         z = 0
-
+    #random.shuffle(data)
+    np.random.shuffle(data)
     #
-    s = 0    
-    Dist_Data = [ 0 for i in range(num_of_clients)]
-    for i in range(0,num_of_clients):
-     Dist_Data[i] = data[s:s + Sizes[i]]
-     s += Sizes[i]
+    samp_size = len(data)
+
+    # ######################
+    classes = dict()
+    count = 0
+    y_tr = Y_train   
+    if(np.shape(y_tr)[1] > 1):  
+      y_tr = np.argmax(Y_train,axis=1)
+
+    for cl in range(num_of_classes):
+        class_ = list()
+        for i in range(len(y_tr)):
+           if( y_tr[i] == cl ):
+             class_.append(i)
+             count +=1
+        class_name = 'class_{}'.format(cl)     
+        classes.update({class_name : class_})  
+
+    #  
+    np.random.shuffle(client_names)
+    # --------------------------------------------
+    # IID
+    if(dist_type == 0):
+       alpha = 0
+       print('... IID Dataset...')
    
-    '''
-    size = len(data)//num_of_clients
-    Dist_Data = [data[i:i + size] for i in range(0, size*num_of_clients, size)]
-    '''
+    # -------------------------------------------- 
+    # non-IID-ie 
+    if(dist_type == 0 or dist_type == 1):
+      
+      if(dist_type == 1):
+        print('... non-IID-ie Dataset...')
+      # the minimum sample size is 0.2% of total samples
+      min_sample_size = int(len(data)*2//1000 )
+      Sizes = [min_sample_size for i in range(0,num_of_clients)] 
+
+      R_samps = len(data) - (min_sample_size * num_of_clients )
+      #z = np.random.randint(0,num_of_clients)
+     
+     # z = 0
+     # while R_samps > (len(data)*0.001):
+     #    clientID = z # np.random.randint(z,num_of_clients)
+     #    samp_size = int(np.round( R_samps * max_sample_size * np.random.rand()))
+     #    Sizes[clientID] += samp_size     
+     #    R_samps = R_samps - samp_size
+     #    z += 1
+     #    if z == 100 :
+     #       z = 0
+
+      d = tf.random.normal(shape=(1,num_of_clients),stddev=alpha)
+      #print(d)
+      dist = (tf.keras.activations.softmax(d) * R_samps +  min_sample_size)
+     
+      #print(dist)
+      dist = np.reshape(dist,(num_of_clients,))
+      #dist = dist +  min_sample_size
+
+      x = np.linspace(1,num_of_clients,num=num_of_clients)
+      plt.bar(x,dist)
+      plt.show()
+    #
+      s = 0    
+      Dist_Data = [ 0 for i in range(num_of_clients)]
+      for i in range(0,num_of_clients):
+         Dist_Data[i] = data[s:s + int(dist[i])]
+         s += int(dist[i])
+
+      q_dist = dist
+   # --------------------------------------------
+    #non-IID-ii 
+    if(dist_type == 2): #(c >= 0.1):
+      print('... non-IID-ii Dataset...')
+    #  c = 0.0001
+      alpha = np.max([alpha,0.001])
+      x = tf.random.normal(shape=(num_of_classes, num_of_clients),stddev=1)
+
+      exp_ = np.zeros((num_of_classes,num_of_clients))
+      dist = np.zeros((num_of_classes,num_of_clients))
+      s = 0
+      for i in range(num_of_classes):
+        for j in range(num_of_clients):
+           exp_[i,j] = np.math.exp(x[i,j])
     
+      s = tf.reduce_sum(exp_)
+ 
+      for i in range(num_of_classes):
+        for j in range(num_of_clients):
+           dist[i][j] = exp_[i][j] / s 
+
+      for i in range(num_of_classes):
+        for j in range(num_of_clients):
+            exp_[i,j] = np.math.exp(dist[i,j]* (num_of_clients*(alpha*10)))
+
+                   
+      for i in range(num_of_classes ):
+         s = tf.reduce_sum(exp_[i,:])
+         class_name = 'class_{}'.format(i)
+         temp_samp = classes[class_name] 
+         rem_samp = len(temp_samp)
+      #  print(s)
+         for j in range(num_of_clients):            
+            dist[i][j] =  int(np.round(exp_[i][j] / s  * len(temp_samp)))
+            if(dist[i][j] > rem_samp):
+               dist[i][j] = rem_samp
+            rem_samp = rem_samp - dist[i][j]  
+
+      Dist_Data = [ 0 for i in range(num_of_clients)]
+      for j in range(num_of_clients):
+         for i in range(num_of_classes):
+           samp_clss = int(dist[i][j])
+           class_name = 'class_{}'.format(i)
+           temp_samp = classes[class_name] 
+           Dist_Data[j] = [data[temp_samp.pop()] for z in range(samp_clss)]
+           classes.update({class_name : temp_samp})
+    # ----------------------------------------
+      q_dist = [np.sum(dist[:,i]) for i in range(num_of_clients)]
+    
+
+    # non-IID-ei
+    if( dist_type == 3):
+       print('... non-IID-ei Dataset...')
+       dist = np.zeros((num_of_classes,num_of_clients))
+       clss = int(np.max([(num_of_classes * (1-alpha)) , 1]))
+       print('classes : {}'.format(clss))
+       Dist_Data = [ 0 for i in range(num_of_clients)]
+       '''
+       samp_clss = len(data) // num_of_clients // clss
+       print('samp_clss : {}'.format(samp_clss))
+       rem_samp = [ len(classes['class_{}'.format(i)])   for i in range(num_of_classes)]
+       
+       for i in range(num_of_clients):
+          temp_sum = 0
+          for j in range(num_of_classes): 
+             if(rem_samp[j] >=  samp_clss and temp_sum < (samp_clss * clss)):
+                dist[j][i] = samp_clss
+                rem_samp[j] = rem_samp[j] - dist[j][i] 
+                temp_sum = temp_sum + samp_clss 
+                class_name = 'class_{}'.format(j)
+                temp_samp = classes[class_name] 
+                Dist_Data[i] = [data[temp_samp.pop()] for z in range(samp_clss)]
+                classes.update({class_name : temp_samp})
+       '''
+# -------
+       samp_clss = int((len(X_train)// num_of_clients // num_of_classes )) 
+       rem_samp = [ len(classes['class_{}'.format(i)])   for i in range(num_of_classes)]
+       clsses = [0 for k in range(clss)]
+       client_clsses = dict()
+       clients_id = [k for k in range(num_of_clients)]
+       np.random.shuffle(clients_id)
+       for i in clients_id:
+          temp_sum = 0
+          clss_ids = [k for k in range(num_of_classes)]
+          clsses = [0 for k in range(clss)]
+          for j in range(clss):
+             r = np.argmax(rem_samp)  #np.random.randint(0,len(clss_ids))
+             id = r#clss_ids.pop(r)
+             clsses[j] = id
+             if(rem_samp[id] >=  samp_clss and temp_sum < (samp_clss * clss)):
+               dist[id][i] = samp_clss
+               rem_samp[id] = rem_samp[id] - dist[id][i] 
+               temp_sum = temp_sum + samp_clss
+               class_name = 'class_{}'.format(id)
+               temp_samp = classes[class_name] 
+               Dist_Data[i] = [data[temp_samp.pop()] for z in range(samp_clss)]
+               classes.update({class_name : temp_samp})
+            
+          client_clsses.update({client_names[i] : clsses })
+
+       samp_clss = int(((len(X_train)// num_of_clients) - samp_clss * clss) / clss)# int((len(X_train)// num_of_clients // num_of_classes ) * (1 / clss))
+       for i in range(num_of_clients):
+          temp_sum = 0
+          clsses = client_clsses[client_names[i]]
+          for j in clsses:
+             if(rem_samp[j] >=  samp_clss and temp_sum < (samp_clss * clss)):
+                rem_samp[j] = rem_samp[j] - samp_clss 
+                dist[j][i] += samp_clss
+                temp_sum = temp_sum + samp_clss
+                class_name = 'class_{}'.format(j)
+                temp_samp = classes[class_name] 
+                Dist_Data[i] = [data[temp_samp.pop()] for z in range(samp_clss)]
+                classes.update({class_name : temp_samp}) 
+
+       q_dist = [np.sum(dist[:,i]) for i in range(num_of_clients)]
+
+
+    if(show_dist):
+       print(np.min(q_dist))
+       print(np.max(q_dist))
+       x = np.linspace(1,num_of_clients,num=num_of_clients)
+       plt.bar(x,q_dist)
+       plt.show()
+       
+
+    # 
     Client_Data = {client_names[i] : Dist_Data[i] for i in range(num_of_clients)} 
     return Client_Data
-
